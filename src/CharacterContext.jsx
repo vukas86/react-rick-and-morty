406126741e36f9a25c6d8d0e786a0aa77dede238
“vukas86” <vukasbit@gmail.com>
2022-12-27 22:03:08 +0100

    working on errors msgs

--- src/CharacterContext.jsx
+++ src/CharacterContext.jsx
@@ -4,28 +4,38 @@ export const CharacterContext = createContext();
 
 export const CharacterProvider = (props) => {
   const [char, setChar] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchCar() {
+    setIsLoading(true);
+    setError(null);
+
     try {
       const response = await fetch(
         "https://rickandmortyapi.com/api/character/[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]"
       );
 
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
       setChar(await response.json());
     } catch (err) {
+      setError(err.message);
       console.error(`Something was wrong! ${err}`);
     }
+    setIsLoading(false);
   }
 
   useEffect(() => {
-    setIsLoading(false);
     fetchCar();
-    setIsLoading(true);
   }, []);
-
+  console.log(error);
   return (
-    <CharacterContext.Provider value={[char, setChar, isLoading, setIsLoading]}>
+    <CharacterContext.Provider
+      value={[char, setChar, isLoading, setIsLoading, error, setError]}
+    >
       {props.children}
     </CharacterContext.Provider>
   );
--- src/components/characterList/CharList.jsx
+++ src/components/characterList/CharList.jsx
@@ -8,20 +8,28 @@ import styles from "./CharList.module.css";
 
 function CharList() {
   let [charInfo, setCharInfo] = useState(null);
-  const [isLoading, setIsLoading] = useContext(CharacterContext);
-  console.log(isLoading);
+  const [char, setChar, isLoading, setIsLoading, error, setError] =
+    useContext(CharacterContext);
+
+  let content = (
+    <Card
+      onChoice={(info) => {
+        setCharInfo(info);
+      }}
+    ></Card>
+  );
+
+  if (error) {
+    content = <p className={styles.errorMsg}>{error}</p>;
+  }
+
+  if (isLoading) {
+    <div className={styles.spinner}></div>;
+  }
 
   return (
     <>
-      <h2 className={styles.title}>Characters</h2>;
-      {isLoading && (
-        <Card
-          onChoice={(info) => {
-            setCharInfo(info);
-          }}
-        />
-      )}
-      {!isLoading && <div class="spinner"></div>}
+      <h2 className={styles.title}>Characters</h2>;{content}
       {charInfo && (
         <Modals
           charInfo={charInfo}
